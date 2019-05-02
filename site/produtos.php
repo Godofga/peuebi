0136abd3c034c9a2b22cdbeb7c18a9fa5d36ab17
Godofga <igor_mcampos@hotmail.com>
2019-05-02 20:46:23 -0300

    werew
    erwre

--- site/produtos.php
+++ site/produtos.php
@@ -6,6 +6,7 @@
 		<title> Visualização de produtos - Prata Shop</title>
 		<meta charset="utf-8" name='viewport' content='width-device-width, initial-scale-1.0'/>
 		<link href='css/bootstrap.min.css' rel ='stylesheet' type='text/css'/>
+		<link rel ='stylesheet' type='text/css' href='css/form.css'>
 		<script type= 'text/javascript' src='js/jquery-3.4.1.min.js'></script>
 		<script type= 'text/javascript' src='js/bootstrap.min.js'></script>
 
@@ -26,38 +27,56 @@
 
 	<body>
 
-		<table>
-			<tr>
-				<th>CATEGORIA</br></th>
-				<th>NOME</th>
-				<th>DESCRICAO</th>
-				<th>IMAGEM</th>
-				<th>PREÇO</th>
-				<th>QUANTIDADE EM ESTOQUE</th>
-			</tr>
-			<?php
-
-				$con = new conexaoDao();
-				$query = "SELECT produto.*, categoria.categoria from produto inner join categoria on (categoria.id=produto.id_categoria)";
-				$resultado =$con->exeSql($query);
-				if($con->exeSql($query,true)){
-					while($row = $resultado->fetch_assoc()){
-						$aux = "imagens/".$row["imagem_produto"];
-						$id = $row["id"];
-						echo "<tr><td>". $row["categoria"]."</td><td>".$row["produto"]."</td><td>".$row["descricao"]."</td><td>"."<a href='compras.php?id=$id'>"."<img src='$aux' alt='é para aparecer' height='100' width='100'>"."</a>"."</td><td>". $row["preco"]."</td><td>".$row["quantidade"]."</td></tr>";
-					}
-
-				}
-
-			 ?>
-		</table>
-
-		<div>
 
 
-				<a href = "main.php"> Voltar à tela inicial </a>
-		</div>
 
-	</body>
+
+		<section class = "container-fluid">
+			<section class= "row justify-content-center">
+
+				<table class="table table-hover" >
+
+						<tr>
+							<th>CATEGORIA</br></th>
+							<th>NOME</th>
+							<th>DESCRICAO</th>
+							<th>IMAGEM</th>
+							<th>PREÇO</th>
+							<th>QUANTIDADE EM ESTOQUE</th>
+						</tr>
+						<?php
+
+							$con = new conexaoDao();
+							$query = "SELECT produto.*, categoria.categoria from produto inner join categoria on (categoria.id=produto.id_categoria)";
+							$resultado =$con->exeSql($query);
+							if($con->exeSql($query,true)){
+								while($row = $resultado->fetch_assoc()){
+									$aux = "imagens/".$row["imagem_produto"];
+									$id = $row["id"];
+									echo "<tr><td>". $row["categoria"]."</td><td>".$row["produto"]."</td><td>".$row["descricao"]."</td><td>"."<a href='compras.php?id=$id'>"."<img src='$aux' alt='é para aparecer' height='100' width='100'>"."</a>"."</td><td>". $row["preco"]."</td><td>".$row["quantidade"]."</td></tr>";
+								}
+
+							}
+
+						 ?>
+				</table>
+						<a href = "index.php" class="alert-link" id="cadastroLinkblack"> Voltar à tela inicial </a>
+				</section>
+		</section>
+
+
+
+
+
+
+
+
+
+
+
+		<div class="container">
+
+</div>
+
 
 </html>
