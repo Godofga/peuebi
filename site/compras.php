90ab73e663d55bd56a533326eaf5329f432939d5
Petrucio2014 <petrucio20111@hotmail.com>
2019-05-02 19:00:34 -0300

    Update compras.php

--- site/compras.php
+++ site/compras.php
@@ -3,7 +3,7 @@
 
 	<head>
 
-		<title> Cadastro de produtos - Prata Shop</title>
+		<title> Compra de produtos - Prata Shop</title>
 		<meta charset="utf-8" name='viewport' content='width-device-width, initial-scale-1.0'/>
 		<link href='css/bootstrap.min.css' rel ='stylesheet' type='text/css'/>
 		<script type= 'text/javascript' src='js/jquery-3.4.1.min.js'></script>
@@ -20,7 +20,7 @@
         {
           header('location:produtos.php');
         } else
-          echo $_GET["id"];
+          echo $_GET['id'];
 
       	checkLogin();
 
@@ -31,7 +31,17 @@
 	<body>
 
      </div>
+        <form action="compras.php?id=<?php echo $_GET["id"]?>" method="POST">
 
+          Quantidade:<br/>
+          <input type="number" name="quantidade"></input><br/>
+          <input type="submit" value= "Realizar compra"/><br/>
+        </form>
+
+        <?php
+          if(isset($_GET['log']) && $_GET['log'])
+            echo "Usuário ou senha incorreta <br>";
+        ?>
 				<a href = "main.php"> Voltar à tela inicial </a>
 		</div>
 
