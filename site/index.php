d8d5a9e9cbe6f5fb438dc917c1644aed4f6c1ec5
Godofga <igor_mcampos@hotmail.com>
2019-05-02 19:21:35 -0300

    sad
    asd

--- site/index.php
+++ site/index.php
@@ -6,6 +6,7 @@
 		<title> Prata shop </title>
 		<meta charset="utf-8" name='viewport' content='width-device-width, initial-scale-1.0'/>
 		<link href='css/bootstrap.min.css' rel ='stylesheet' type='text/css'/>
+		<link rel ='stylesheet' type='text/css' href='css/login.css'>
 		<script type= 'text/javascript' src='js/jquery-3.4.1.min.js'></script>
 		<script type= 'text/javascript' src='js/bootstrap.min.js'></script>
 
@@ -22,23 +23,32 @@
 
 	<body>
 
-		<div>
-			<form action="login.php?do=true" method="POST">
+		<section class = "container-fluid">
+			<section class= "row justify-content-center">
+				<section class="col-l2 col-sm-6 col-md-3">
+					<form class='form-container' action="login.php?do=true" method="POST">
+					  <div class="form-group">
+					    <label for="user">Usuário</label>
+					    <input type="text" class="form-control" id="user" name="usuario" placeholder="Nome de usuário">
+					  </div>
+					  <div class="form-group">
+					    <label for="senha">Senha</label>
+					    <input type="password" class="form-control" id="senha" name="senha" placeholder="Senha">
+					  </div>
+						<?php
+							if(isset($_GET['log']) && $_GET['log'])
+								echo "
+									<div class='alert alert-danger' role='alert'>
+									  Usuário ou senha incorreta!
+									</div>";
+						?>
 
-				Usuário:<br/>
-				<input type="text" name="usuario"></input><br/>
-				Senha:<br/>
-				<input type="password" name="senha"></input><br/>
-				<input type="submit" value= "Realizar login"/><br/>
-			</form>
-
-				<?php
-					if(isset($_GET['log']) && $_GET['log'])
-						echo "Usuário ou senha incorreta <br>";
-				?>
-
-				<a href = "cadastro.php"> Cadastrar-se </a>
-		</div>
+					  <button type="submit" class="btn btn-outline-dark btn-block">Realizar login</button><br>
+						<a href = "cadastro.php" class="alert-link" id="cadastroLink"> Cadastrar-se </a>
+					</form>
+				</section>
+			</section>
+		</section>
 
 	</body>
 
