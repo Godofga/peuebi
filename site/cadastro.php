0529318e6c6674247600261c5c0164259ae092f9
Godofga <igor_mcampos@hotmail.com>
2019-05-02 19:52:03 -0300

    qewr
    werwer

--- site/cadastro.php
+++ site/cadastro.php
@@ -6,53 +6,82 @@
 		<title> Cadastro - Prata Shop</title>
 		<meta charset="utf-8" name='viewport' content='width-device-width, initial-scale-1.0'/>
 		<link href='css/bootstrap.min.css' rel ='stylesheet' type='text/css'/>
+		<link rel ='stylesheet' type='text/css' href='css/form.css'>
 		<script type= 'text/javascript' src='js/jquery-3.4.1.min.js'></script>
 		<script type= 'text/javascript' src='js/bootstrap.min.js'></script>
 
-    <?php
-      require 'conexao.php';
 
-        if(isset($_POST['nome'])&&isset($_POST['usuario'])&&isset($_POST['senha'])&&isset($_POST['email'])&&isset($_POST['cpf'])&&isset($_POST['cidade'])&&isset($_POST['estado'])&&isset($_POST['bairro'])){
-
-          $user = new usuario($_POST['cpf'],$_POST['nome'],$_POST['email'],$_POST['usuario'],$_POST['senha'],0,$_POST['estado'],$_POST['cidade'],$_POST['bairro']);
-          if($user->salvar())
-          	echo 'Feito!<br>';
-          else
-          	echo "Não feito";
-
-        }
-
-    ?>
 
 	</head>
 
 	<body>
 
-		<div>
-			<form action = "cadastro.php" method="POST">
 
-        		Nome:<br/>
-				<input type="text" name="nome"></input><br/>
-				Usuário:<br/>
-				<input type="text" name="usuario"></input><br/>
-				Senha:<br/>
-				<input type="password" name="senha"></input><br/>
-        		E-mail:<br/>
-				<input type="email" name="email"></input><br/>
-    		    CPF:<br/>
-				<input type="text" name="cpf"></input><br/>
-				Estado:<br/>
-				<input type="text" name= "estado"></input><br/>
-    		    Cidade:<br/>
-				<input type="text" name="cidade"></input><br/>
-				Bairro:<br/>
-				<input type="text" name="bairro"></input><br/>
+		<section class = "container-fluid">
+			<section class= "row justify-content-center">
+				<section class="col-l2 col-sm-6 col-md-3">
+					<form class='form-container' action="cadastro.php" method="POST">
+					  <div class="form-group">
+					    <label for="nome">Nome</label>
+					    <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome do indivíduo">
+					  </div>
+						<div class="form-group">
+					    <label for="usuario">Usuário</label>
+					    <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Nome de usuário">
+					  </div>
+						<div class="form-group">
+					    <label for="senha">Senha</label>
+					    <input type="password" class="form-control" id="senha" name="senha" placeholder="Senha">
+					  </div>
+						<div class="form-group">
+					    <label for="e_mail">Email</label>
+					    <input type="email" class="form-control" id="e_mail" name="email" placeholder="E-mail">
+					  </div>
+						<div class="form-group">
+					    <label for="cpf">CPF</label>
+					    <input type="text" class="form-control" id="cpf" name="cpf" placeholder="CPF do indivíduo">
+					  </div>
+						<div class="form-group">
+					    <label for="estado">Estado</label>
+					    <input type="text" class="form-control" id="estado" name="estado" placeholder="Estado">
+					  </div>
+						<div class="form-group">
+					    <label for="cidade">Cidade</label>
+					    <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Cidade">
+					  </div>
+						<div class="form-group">
+					    <label for="bairro">Bairro</label>
+					    <input type="text" class="form-control" id="bairro" name="bairro" placeholder="Bairro">
+					  </div>
 
-				<input type="submit" value= "Realizar cadastro"/><br/>
-			</form>
+						<?php
+				      require 'conexao.php';
 
-				<a href = "index.php"> Voltar à tela de login </a>
-		</div>
+				        if(isset($_POST['nome'])&&isset($_POST['usuario'])&&isset($_POST['senha'])&&isset($_POST['email'])&&isset($_POST['cpf'])&&isset($_POST['cidade'])&&isset($_POST['estado'])&&isset($_POST['bairro'])){
+
+				          $user = new usuario($_POST['cpf'],$_POST['nome'],$_POST['email'],$_POST['usuario'],$_POST['senha'],0,$_POST['estado'],$_POST['cidade'],$_POST['bairro']);
+				          if($user->salvar())
+				          	echo "
+											<div class='alert alert-success' role='alert'>
+											  Cadastro concluído!
+											</div>";
+				          else
+				          	echo "
+											<div class='alert alert-warning' role='alert'>
+											  Algo de errado ocorreu!
+											</div>";
+
+				        }
+
+				    ?>
+
+					  <button type="submit" class="btn btn-outline-dark btn-block">Cadastrar-se</button><br>
+						<a href = "index.php" class="alert-link" id="cadastroLink"> Voltar à tela de login </a>
+					</form>
+				</section>
+			</section>
+		</section>
+
 
 	</body>
 
