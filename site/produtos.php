46203d34646cc932c3ea988ddb348324f3bc6997
Petrucio2014 <petrucio20111@hotmail.com>
2019-05-02 18:27:32 -0300

    aaa

--- site/produtos.php
+++ site/produtos.php
@@ -42,7 +42,7 @@
 				$resultado =$con->exeSql($query);
 				if($con->exeSql($query,true)){
 					while($row = $resultado->fetch_assoc()){
-						$aux = $row["imagem_produto"];
+						$aux = "imagens/".$row["imagem_produto"];
 						echo "<tr><td>". $row["categoria"]."</td><td>".$row["produto"]."</td><td>".$row["descricao"]."</td><td>"."<img src='$aux' alt='é para aparecer' height='100' width='100' onclick='compras.php?id=true'>"."</td><td>". $row["preco"]."</td><td>".$row["quantidade"]."</td></tr>";
 					}
 
