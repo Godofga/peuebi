10e47a17427bbea739b19abe0bf5e80cd3a527f9
Godofga <igor_mcampos@hotmail.com>
2019-05-02 21:50:16 -0300

    wef
    few

--- site/status.php
+++ site/status.php
@@ -6,6 +6,8 @@
 		<title> Alteração de status - Prata Shop</title>
 		<meta charset="utf-8" name='viewport' content='width-device-width, initial-scale-1.0'/>
 		<link href='css/bootstrap.min.css' rel ='stylesheet' type='text/css'/>
+
+		<link rel ='stylesheet' type='text/css' href='css/form.css'>
 		<script type= 'text/javascript' src='js/jquery-3.4.1.min.js'></script>
 		<script type= 'text/javascript' src='js/bootstrap.min.js'></script>
 
@@ -28,7 +30,7 @@
           $con = new conexaoDao();
 					$id = $_POST['pedido'];
 					$status = $_POST['gender'];
-					
+
 					if($con->exeSql("select * from pedido where id = $id",true)){
 						$con->exeSql("update pedido set situacao = '$status' where id = $id");
           	echo 'Feito!<br/>';
@@ -45,44 +47,64 @@
 
 	<body>
 
-		<table>
-			<tr>
-				<th>ID</th>
-				<th>CPF</th>
-				<th>MOMENTO</th>
-				<th>SITUAÇÃO</th>
-			</tr>
-			<?php
+		<section class = "container-fluid">
+			<section class= "row justify-content-center">
 
-				$con = new conexaoDao();
-				$query = "SELECT * from pedido where situacao = 'pendente'";
-				$resultado =$con->exeSql($query);
-				if($con->exeSql($query,true)){
-					while($row = $resultado->fetch_assoc()){
-						echo "<tr><td>". $row["id"]."</td><td>".$row["cpf_cliente"]."</td><td>".$row["momento"]."</td><td>".$row["situacao"]."</td></tr>";
-					}
+				<table class="table table-hover" >
 
-				}
+					<tr>
+						<th>ID</th>
+						<th>CPF</th>
+						<th>MOMENTO</th>
+						<th>SITUAÇÃO</th>
+					</tr>
+					<?php
 
-			 ?>
-		</table>
+						$con = new conexaoDao();
+						$query = "SELECT * from pedido where situacao = 'pendente'";
+						$resultado =$con->exeSql($query);
+						if($con->exeSql($query,true)){
+							while($row = $resultado->fetch_assoc()){
+								echo "<tr><td>". $row["id"]."</td><td>".$row["cpf_cliente"]."</td><td>".$row["momento"]."</td><td>".$row["situacao"]."</td></tr>";
+							}
 
-		<div>
-			<form action = "status.php" method="POST">
-				<br>
-				Id do pedido para alteração:<br/><br>
-				<input type="number" name="pedido"></input><br/><br>
-        		Novo estado do pedido:<br/><br>
-				<input type="radio" name="gender" value = "Aprovado"> Aprovar<br><br>
-				<input type="radio" name="gender" value = "Negado"> Negar<br>
-				<br>
+						}
 
-				<input type="submit" value= "Realizar alteração"/><br/>
-			</form>
+					 ?>
+				</table>
+						<a href = "main.php" class="alert-link" id="cadastroLinkblack"> Voltar à tela inicial </a>
+				</section>
+		</section>
 
-				<a href = "main.php"> Voltar à tela inicial </a>
-		</div>
-
+		<section class = "container-fluid">
+			<section class= "row justify-content-center">
+					<form class='form-container' action = "status.php" method="POST">
+						<div class="form-group">
+							<label for="pedido">Id do pedido</label>
+							<input type="number" class="form-control" id="pedido" name="pedido" placeholder="Id do pedido para alteração">
+						</div>
+						<div class="btn-group btn-group-toggle" data-toggle="buttons">
+						  <label class="btn btn-secondary active">
+						    <input type="radio" name="gender" id="option1" autocomplete="off" checked value = "Aprovado"> Aprovar
+						  </label>
+						  <label class="btn btn-secondary">
+						    <input type="radio" name="gender" id="option2" autocomplete="off" value = "Negado"> Negar
+						  </label>
+						</div>
+						<br>
+						<?php
+							if(isset($_GET['log']) && $_GET['log'])
+								echo "
+									<div class='alert alert-danger' role='alert'>
+										Usuário ou senha incorreta!
+									</div>";
+						?>
+						<br>
+						<button type="submit" class="btn btn-outline-dark btn-block">Realizar alteração</button><br>
+						<a href = "main.php" class="alert-link" id="cadastroLink"> Retornar à tela inicial </a>
+					</form>
+				</section>
+		</section>
 	</body>
 
 </html>
